fba97bc06db4f8e6305039706ce66a6e3e84ff4b
Jett in CS <chihieu.ntu@gmail.com>
2026-04-18 08:40:26 +0700

    Small upgrade

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  allowedDevOrigins: ["192.168.1.222"],
+  allowedDevOrigins: ["192.168.1.2"],
   typescript: {
     ignoreBuildErrors: true,
   },
